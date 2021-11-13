00e9a3105119407730c2cff0f0b10a406e748661
Jeremy-Ramirez <66746920+Jeremy-Ramirez@users.noreply.github.com>
2021-11-13 16:41:42 -0500

    Implementando palabras reservadas

---
+++ main.py
@@ -0,0 +1,37 @@
+import ply.lex as lex
+
+
+
+reserved = {
+    'class':'CLASS',
+    'and':'AND',
+    'break':'BREAK',
+    'case': 'CASE',
+    'def':'DEF',
+    'defined?':'DEFINED?',
+    'do':'DO',
+    'else': 'ELSE',
+    'elsif':'ELSIF',
+    'end':'END',
+    'ensure':'ENSURE',
+    'false':'FALSE',
+    'true':'TRUE',
+    'for':'FOR',
+    'if':'IF',
+    'in':'IN',
+    'module':'MODULE',
+    'next':'NEXT',
+    'nil':'NIL',
+    'not':'NOT',
+    'or':'OR',
+    'redo':'REDO',
+    'rescue':'RESCUE',
+    'return':'RETURN',
+    'self':'SELF',
+    'until':'UNTIL',
+    'when':'WHEN',
+    'while':'WHILE',
+    'undef':'UNDEF'
+   
+}
+
