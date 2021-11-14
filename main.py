5aadfe9b2ea380cf6bfdbeec58e25737de8f0a10
diacrami <diacrami@espol.edu.ec>
2021-11-13 21:11:53 -0500

    Strings y comentarios

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import ply.lex as lex
 
 tokens = [
     'NUMBER',
-    'BOOLEAN', 
     'STRINGV',
     'IDENTIFIER',
     'COMPARE',
@@ -34,12 +33,6 @@ tokens = [
     'NOTEQUAL',
     'COMIS',
     'COMID'
-  
-    
-    
-    
-
-
 ]
 
 
@@ -50,7 +43,7 @@ reserved = {
     'break':'BREAK',
     'case': 'CASE',
     'def':'DEF',
-    'defined?':'DEFINED?',
+    'defined':'DEFINED',
     'do':'DO',
     'else': 'ELSE',
     'elsif':'ELSIF',
@@ -78,11 +71,82 @@ reserved = {
     'gets':'GETS',
     'print':'PRINT',
     'putc':'PUTC',
-    'array':'ARRAY',
-    'hash':'HASH'
-
-   
+    'Array':'ARRAY',
+    'Hash':'HASH',
+    'String':'STRING',
+    'new':'NEW'
 }
 
 
 tokens += list(reserved.values())
+
+
+#t_STRINGV = r'((\'[\w\W\s][^\']*\')|(\"[\w\W\s][^\"]*\"))'
+t_COMPARE = r'==='
+t_MINUS = r'-'
+t_PLUS = r'\+'
+t_DIVISION = r'\/'
+t_MOD = r'%'
+t_MULT = r'\*'
+t_EXP = r'\*\*'
+t_LESS = r'<'
+t_GREATER = r'>'
+t_RPARENTHESIS = r'\)'
+t_LPARENTHESIS = r'\('
+t_ASSING = r'='
+t_POINT = r'\.'
+t_COMMA = r','
+t_SEMICOLON = r';'
+t_LSQBRACKET = r'\['
+t_RSQBRACKET = r'\]'
+t_LBRACKET = r'{'
+t_RBRACKET = r'}'
+t_NOTS = r'!'
+t_ANDS = r'&&'
+t_ORS = r'\|\|'
+t_GREQUAL = r'>='
+t_LSEQUAL = r'<='
+t_NOTEQUAL = r'!='
+
+
+def t_STRINGV(t):
+    r'((\'[\w\W\s][^\']*\')|(\"[\w\W\s][^\"]*\"))'
+    t.value= str(t.value)
+    return t
+
+#def t_NUMBER():
+
+def t_IDENTIFIER(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value, 'IDENTIFIER')
+    return t
+    
+
+def t_COMMENT(t):
+    r'((?!.*\n)\#.*)|(\=begin(\n|.)*\=end)'
+    pass
+
+t_ignore = " \t" # Qué tipo de caracteres debe ignorar Ruby?
+
+def t_NEWLINE(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+
+
+
+lexer = lex.lex()
+
+
+def inputLex(s):
+    lexer.input(s)
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+        print(tok)
+
+file = open("archivo.txt", encoding="utf8")  
+
+for line in file:
+    inputLex(line)
